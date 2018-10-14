5dac07cb70c3ae2a18e4d81880a4d94f738c315a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-14 15:23:21 -0400

    Saving mergeUSA.R

--- mergeUSA.R
+++ mergeUSA.R
@@ -1,1 +1,19 @@
 
+library("ggmap")
+library("ggplot2")
+
+arrests <- USArrests
+
+arrests
+
+df1<-df
+
+arrests$Statename <- rownames(arrests)
+
+arrests
+
+#3) Create a merged dataframe -- with the attributes from both dataset
+
+merge_df <- merge(arrests,df,by="Statename")
+
+merge_df
