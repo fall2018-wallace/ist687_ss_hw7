4c4abbbbbd279d810821cc78114ebf493ac4bf72
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-14 22:03:58 -0400

    Saving mergeUSA.R

--- mergeUSA.R
+++ mergeUSA.R
@@ -18,7 +18,8 @@ merge_df <- merge(arrests,df,by="Statename")
 merge_df
 
 #2)	Add the area of each state, and the center of each state,
-#   to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
+#   to the merged dataframe, using the ‘state.center’, ‘state.center’ 
+#   and ‘state.name’ vectors
 
 area<- state.area
 center<-state.center
